b5b3b1e51f060605b532a409557fab57edd429be
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-07-13 12:10:26 +0200

    Breaking

---
+++ Meteo-exercise/Meteo-exercise/Meteo_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_exerciseApp.swift
+//  Meteo-exercise
+//
+//  Created by Antonio Scognamiglio on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
