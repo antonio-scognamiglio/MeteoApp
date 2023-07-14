413e8301aa7cbe2291216aaf5345a8aeb83ea84c
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-07-14 20:15:51 +0200

    formattedCoordinates

--- Meteo-exercise/ContentView.swift
+++ Meteo-exercise/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     @State var weatherForecast: WeatherForecast?
     
+    var strCoordinates: (latitude: String, longitude: String) {
+        if let lat = (weatherForecast?.latitude), let long = (weatherForecast?.longitude) {
+            let stringLat = String(format: "%.2f", lat)
+            let stringLong = String(format: "%.2f", long)
+            return (stringLat, stringLong)
+        } else { return ("", "") }
+    }
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Latitudine: \(strCoordinates.latitude)")
+                .fontWeight(.semibold)
+                .font(.title)
+            Text("Longitudine: \(strCoordinates.longitude)")
+                .font(.title)
+                .fontWeight(.semibold)
         }
         .padding()
         .task {
