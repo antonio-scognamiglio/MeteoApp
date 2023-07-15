3c83ef007dfe126aa4a0e3f9f41cb36f495876d0
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-07-15 10:32:12 +0200

    added names and images for weather codes

--- Meteo-exercise/WeatherForecast.swift
+++ Meteo-exercise/WeatherForecast.swift
@@ -35,9 +35,52 @@ struct WeatherForecast: Codable {
         switch weatherCode {
         case 0 : return "ClearSky"
         case 1, 2, 3 : return "MainlyClear"
-        
-        default:
-            return "ClearSky"
+        case 45, 48:  return "Fog"
+        case 51, 53, 55: return "Drizzle"
+        case 56, 57: return "FreezingDrizzle"
+        case 61, 63, 65: return "Rain"
+        case 66, 67: return "FreezingRain"
+        case 71, 73, 75: return "Snowfall"
+        case 77: return "SnowGrains"
+        case 80, 81, 82: return "RainShowers"
+        case 85, 86: return "SnowShowers"
+        case 95: return "Thunderstorm"
+        case 96, 99: return "ThunderstormWithSlightAndHeavyHail"
+        default: return "ClearSky"
+        }
+    }
+    
+    func getWeatherName(weatherCode: Int) -> String {
+        switch weatherCode {
+        case 0: return "Clear sky"
+        case 1: return "Mainly clear"
+        case 2: return "Partly cloudy"
+        case 3: return "Overcast"
+        case 45: return "Fog"
+        case 48: return "Depositing rime fog"
+        case 51: return "Drizzle light intensity"
+        case 53: return "Drizzle moderate intensity"
+        case 55: return "Drizzle dense intensity"
+        case 56: return "Freezing Drizzle light intensity"
+        case 57: return "Freezing Drizzle dense intensity"
+        case 61: return "Rain slight intensity"
+        case 63: return "Rain moderate intensity"
+        case 65: return "Rain heavy intensity"
+        case 66: return "Freezing Rain Light intensity"
+        case 67: return "Freezing Rain heavy intensity"
+        case 71: return "Snow fall slight intensity"
+        case 73: return "Snow fall moderate intensity"
+        case 75: return "Snow fall heavy intensity"
+        case 77: return "Snow grains"
+        case 80: return "Rain shower slight"
+        case 81: return "Rain shower moderate"
+        case 82: return "Rain shower violent"
+        case 85: return "Snow showers slight"
+        case 86: return "Snow showers heavy"
+        case 95: return "Thunderstorm: Slight or moderate"
+        case 96: return "Thunderstorm with slight hail"
+        case 99: return "Thunderstorm with heavy hail"
+        default: return "Clear sky"
         }
     }
 }
