154bb6be717a593840bb8a676b987a727aa26150
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-07-15 12:26:56 +0200

    fixing a bug

--- Meteo-exercise/ContentView.swift
+++ Meteo-exercise/ContentView.swift
@@ -8,42 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var weatherForecast: WeatherForecast?
+    @State var weatherForecast = WeatherForecast()
+    @State var hasFetched = false
+    
+//    var strHourlyTemps: [String]{
+//        if let hourlyTemps = weatherForecast.hourly?.temperature2M {
+//            return  hourlyTemps.map { temp in
+//                String(temp)
+//            }
+//        } else {
+//            return ["Empty"]
+//        }
+//    }
     var backgroundColor = #colorLiteral(red: 0.5262038708, green: 0.7596305013, blue: 0.912558496, alpha: 1)
     var backgroundColor2 = #colorLiteral(red: 0.968627451, green: 0.9490196078, blue: 0.8509803922, alpha: 1)
-    var strCoordinates: (latitude: String, longitude: String) {
-        if let lat = (weatherForecast?.latitude), let long = (weatherForecast?.longitude) {
-            let stringLat = String(format: "%.2f", lat)
-            let stringLong = String(format: "%.2f", long)
-            return (stringLat, stringLong)
-        } else { return ("", "") }
-    }
-    
     
     var body: some View {
         ZStack {
             Color(backgroundColor).ignoresSafeArea()
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Latitudine: \(strCoordinates.latitude)")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                    Text("Longitudine: \(strCoordinates.longitude)")
-                        .font(.title2)
-                        .fontWeight(.semibold)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Latitudine: \(weatherForecast.strCoordinates.latitude)")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Longitudine: \(weatherForecast.strCoordinates.longitude)")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    Image(systemName: getImageCode(for: (weatherForecast.daily?.weathercode?.first ?? WeatherForecast.example.daily?.weathercode?.first)!))
+                        .resizable()
+                        .foregroundColor(.yellow)
+                        .scaledToFit()
                 }
-                Spacer()
-                Image(systemName: "sun.and.horizon.circle.fill")
-                    .resizable()
-                    .foregroundColor(.yellow)
-                    .scaledToFit()
+                
+                Text("Today")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                if hasFetched {
+                    ForEach(weatherForecast.strHourlyTemps, id: \.self){ temp in
+                            Text("Temperature: \(temp)")
+                        }
+                }
             }
+                
             .padding()
         }
         .task {
             do {
-               weatherForecast = try await APIHandler.shared.fetchWeatherForecast()
+                weatherForecast = try await APIHandler.shared.fetchWeatherForecast()
+                hasFetched = true
             } catch {
+                hasFetched = false
                 print(error.localizedDescription)
             }
     }
